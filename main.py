a6c4cad8abaff88d50a5c28f7ba17895325339a0
Jnguyen497 <145719204+Jnguyen497@users.noreply.github.com>
2023-09-30 18:24:51 +0000

    testing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     stocks = { 'AAPL': 189.70, 'AMZN': 137.27, 'ATVI': 92.03, 'GOOGL': 135.77, 'HSY': 212.10, 'META': 300.15, 'NFLX': 523.45, 'RBLX': 29.13, 'TSLA': 256.49, 'ZM': 73.28 }
     
     print(f"The current price of {ticker} is ${stocks[ticker]:.2f}") 
-    
+  #update  
   # No Search Found
   except:
     print()
